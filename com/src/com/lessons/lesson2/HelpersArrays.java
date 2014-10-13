ae5697e67178a05ef42c51439864c3a17f9b1b2e
apaliy <anplay@yandex.ru>
2014-10-13 14:06:20 +0300

    HomeWork is Completed

--- com/src/com/lessons/lesson2/HelpersArrays.java
+++ com/src/com/lessons/lesson2/HelpersArrays.java
@@ -4,4 +4,90 @@ package com.lessons.lesson2;
  * Created by dev-anplay on 13.10.2014.
  */
 public class HelpersArrays {
+    public static int getLengthResultArrayForInner(int[] firstArray, int[] secondArray) {
+        int lengthNewArray = 0;
+        for(int i=0; i < firstArray.length; i++){
+            for(int n=0; n < secondArray.length; n++){
+                if (firstArray[i] == secondArray[n]){
+                    lengthNewArray++;
+                    break;
+                }
+            }
+        }
+        return lengthNewArray;
+    }
+
+    public static int[] innerArray(int[] firstArray, int[] secondArray){
+        int lengthNewArray = getLengthResultArrayForInner(firstArray, secondArray);
+        int[] resultArray = new int[lengthNewArray];
+        int first = 0;
+        for(int i=0; i < firstArray.length; i++){
+            for(int n=0; n < secondArray.length; n++){
+                if (firstArray[i] == secondArray[n]){
+                    resultArray[first] = firstArray[i];
+                    first++;
+                    break;
+                }
+            }
+        }
+        return resultArray;
+    }
+
+    public static int getLengthResultArrayForOuter(int[] firstArray, int[] secondArray) {
+        int lengthNewArray = 0;
+        for(int i=0; i < firstArray.length; i++){
+            boolean check = false;
+            for(int n=0; n < secondArray.length; n++){
+                if (firstArray[i] == secondArray[n]){
+                    check = true;
+                    break;
+                }
+            }
+            if (!check) lengthNewArray++;
+        }
+        for(int i=0; i < secondArray.length; i++){
+            boolean check = false;
+            for(int n=0; n < firstArray.length; n++){
+                if (firstArray[n] == secondArray[i]){
+                    check = true;
+                    break;
+                }
+            }
+            if (!check) lengthNewArray++;
+        }
+        return lengthNewArray;
+    }
+
+    public static int[] outerArray(int[] firstArray, int[] secondArray){
+        int lengthNewArray = getLengthResultArrayForOuter(firstArray, secondArray);
+        int[] resultArray = new int[lengthNewArray];
+        int first = 0;
+        for(int i=0; i < firstArray.length; i++){
+            boolean check = false;
+            for(int n=0; n < secondArray.length; n++){
+                if (firstArray[i] == secondArray[n]){
+                    check = true;
+                    break;
+                }
+            }
+            if (!check) {
+                resultArray[first] = firstArray[i];
+                first++;
+            };
+        }
+        for(int i=0; i < secondArray.length; i++) {
+            boolean check = false;
+            for (int n = 0; n < firstArray.length; n++) {
+                if (firstArray[n] == secondArray[i]) {
+                    check = true;
+                    break;
+                }
+            }
+            if (!check) {
+                resultArray[first] = secondArray[i];
+                first++;
+            };
+        }
+        return resultArray;
+    }
 }
--- com/src/com/lessons/lesson2/Main.java
+++ com/src/com/lessons/lesson2/Main.java
@@ -1,10 +1,18 @@
 package com.lessons.lesson2;
 
+import java.util.Arrays;
+
 /**
  * Created by dev-anplay on 13.10.2014.
  */
 public class Main {
-    public static void main(String[] args) {
+    private static int[] firstArray = {1,5,4,23,65,32,78};
+    private static int[] secondArray = {3,5,24,4,1,2,34,45,32,5};
 
+    public static void main(String[] args) {
+        int[] resultArrayForInner = HelpersArrays.innerArray(firstArray, secondArray);
+        System.out.println("Inner Array - " + Arrays.toString(resultArrayForInner));
+        int[] resultArrayForOuter = HelpersArrays.outerArray(firstArray, secondArray);
+        System.out.println("Outer Array - " + Arrays.toString(resultArrayForOuter));
     }
 }
